2c17a9190802464217c075d91b593a7889334919
Artyom Galimzyanov <artyomgalimzyanov@MacBook-Artyom.local>
2021-11-02 19:11:34 +0300

    changes 2 Nov

--- contracts/StepCoin.sol
+++ contracts/StepCoin.sol
@@ -1,27 +1,63 @@
 // SPDX-License-Identifier: MIT
-pragma solidity >=0.4.22 <0.9.0;
+//pragma solidity >=0.4.21 <0.8.9;
+pragma solidity 0.8.0;
+pragma experimental ABIEncoderV2;
 
-contract StepCoin {
+import "@openzeppelin/contracts/access/Ownable.sol";
 
-  // state variables
-  address public owner = msg.sender;
+/// @title Contract StepCoin allows to convert your steps into coins and get award
+/// @author Artem Galimzyanov
+/// @notice Allows a user to convert steps into coins and  get award
+
+
+contract StepCoin is Ownable {
+
+/// state variables
   uint public awardCount;
+  bool public paused = false;
 
-  // enrollment to the StepCoin
+
+modifier whenNotPaused () {
+    require (!paused);
+    _;
+  }
+
+modifier whenPaused () {
+    require (paused);
+    _;
+  }
+
+modifier paidEnough(uint _i) {
+    require(balances[msg.sender]>= awards[_i].price, "Not enough Stepcoins");
+    _;
+  }
+  
+modifier forSale(uint _i) {
+    require(awards[_i].state == State.New && awards[_i].seller != address(0));
+    _;
+  }
+modifier Redeemed(uint _i) {
+    require(awards[_i].state == State.Redeemed && awards[_i].seller != address(0));
+    _;
+  }
+
+  /// enrollment to the StepCoin
   mapping (address => bool) public enrolled;
-  // keep clients balances of stepcoints
-  mapping (address => uint) internal balances;
-  // mapping all awards from shops
+  /// keep clients balances of stepcoints
+  mapping (address => uint) public balances;
+  /// mapping all awards from shops
   mapping (uint256 => Award) public awards;
+  /// mapping of awards per user
+  mapping (address => string[]) public  List;
 
-  // enum states of award: New, Redeemed, Used
+  /// enum states of award: New, Redeemed, Used
   enum State {
     New,
     Redeemed,
     Used
   }
 
-  // struct Award
+  /// struct Award
   struct Award {
     string name;
     uint count;
@@ -31,58 +67,91 @@ contract StepCoin {
     address payable buyer;
   }
 
-  // Events
+  ///Events
   event LogEnrolled(address accountAddress);
   event LogBalance(address accountAddress, uint amount);
   event LogRedeem(address accountAddress, uint redeemAmount, uint newBalance);
+  event Pause();
+  event Unpause();
 
-// function to enroll to the system
-  function enroll() public returns(bool){
+
+  constructor() public {
+  
+  }
+
+// function to enroll to the Program
+  function enroll() public whenNotPaused returns(bool){
     enrolled[msg.sender] = true;
     emit LogEnrolled(msg.sender);
     return enrolled[msg.sender];
   }
 
-// fuction to deposit steps into coins
-// maybe deposit should take steps from user and convert to coint??
-  function depositSteps() public payable returns (uint) {
-    require(enrolled[msg.sender]== true);
-    balances[msg.sender] +=msg.value;
+/// function to deposit steps and get Stepcoins
+  function depositSteps(uint step) public payable whenNotPaused returns (uint) {
+    require(enrolled[msg.sender]== true, "Please enroll");
+    balances[msg.sender] += step;
     emit LogBalance(msg.sender, balances[msg.sender]);
     return balances[msg.sender];
   }
 
-// function to check balance of coins
-  function checkBalance() public view returns(uint){
-    return balances[msg.sender];
+/// function to check balance of Users awards
+  function checkmyAwards() public view returns(string[] memory){
+    return List[msg.sender];
   }
 
-// function to add Award to the site
-  function addAward(string memory _name, uint _price) public returns(bool){
+/// function to add Award to the site
+  function addAward(string memory _name, uint _price) public whenNotPaused returns(bool){
     awards[awardCount] = Award({
       name: _name,
       count: awardCount,
       price: _price,
       state: State.New,
-      seller: msg.sender,
-      buyer: address(0)
+      seller: payable (msg.sender),
+      buyer: payable ( address(0))
     });
+    
     awardCount = awardCount +1;
     return true;
   }
 
-// function to redeem stepcoin and get Award
-// maybe need to add some modifier like Award is New and Buyer has enough coins to redeem
-  function getAward(uint _awardCount) payable public {
-    awards[_awardCount].buyer = msg.sender;
-    awards[_awardCount].state = State.Redeemed;
-    awards[_awardCount].seller.transfer(awards[_awardCount].price);
-    uint newBalance = balances[msg.sender]-awards[_awardCount].price;
-    emit LogRedeem(awards[_awardCount].buyer, awards[_awardCount].price, newBalance);
-  } 
+/// function to buy Award with StepCoins  
+function buyAward(uint _i)  public payable forSale(_i)  paidEnough(_i) whenNotPaused returns (uint){
+    awards[_i].buyer = payable (msg.sender);
+    awards[_i].state = State.Redeemed;
+    balances[msg.sender] -= awards[_i].price;
+    List[msg.sender].push(awards[_i].name);
+    emit LogRedeem(awards[_i].buyer, awards[_i].price, balances[msg.sender]);
+    return balances[msg.sender];
+}
 
-// function to use Award
-  function useAward(uint _awardCount) payable public{
-    awards[_awardCount].state = State.Used;
+/// function to use Award
+  function useAward(uint _i) public payable whenNotPaused Redeemed(_i) returns(bool){
+    require (awards[_i].seller == msg.sender, "You are not a seller");
+    awards[_i].state = State.Used;
+    return true;
   }
+
+  function pause() public onlyOwner whenNotPaused returns(bool){
+    paused = true;
+    emit Pause();
+    return true;
+  }
+
+  function unpause() public onlyOwner whenPaused {
+    paused = false;
+    emit Unpause();
+  }
+
+  function fetchAward(uint _i) public view 
+    returns (string memory name, uint count, uint price, uint state, address seller, address buyer) 
+  {
+    name = awards[_i].name;
+    count = awards[_i].count;
+    price = awards[_i].price;
+    state = uint(awards[_i].state);
+    seller = awards[_i].seller;
+    buyer = awards[_i].buyer;
+    return (name, count, price, state, seller, buyer);
+  }
+
 }
--- react_test/client/src/App.js
+++ react_test/client/src/App.js
@@ -1,73 +1,89 @@
-import React, { Component } from "react";
-import SimpleStorageContract from "./contracts/SimpleStorage.json";
-import getWeb3 from "./getWeb3";
-
-import "./App.css";
+import React, { Component, useState} from 'react';
+import StepCoinContract from "./contracts/StepCoin.json";
+import Web3 from 'web3'
+import './App.css'
 
 class App extends Component {
-  state = { storageValue: 0, web3: null, accounts: null, contract: null };
 
-  componentDidMount = async () => {
-    try {
-      // Get network provider and web3 instance.
-      const web3 = await getWeb3();
+  constructor(props) {
+    super(props)
+    this.state = { 
+      account: '',
+      isEnroll: false,
+      currentBalance: 0,
+      count: 0
+     };
+  }
+  
+  componentDidMount() {
+    this.loadBlockchainData();
+    this.Enroll();
+  
+  }
 
-      // Use web3 to get the user's accounts.
-      const accounts = await web3.eth.getAccounts();
 
-      // Get the contract instance.
-      const networkId = await web3.eth.net.getId();
-      const deployedNetwork = SimpleStorageContract.networks[networkId];
-      const instance = new web3.eth.Contract(
-        SimpleStorageContract.abi,
+
+  async loadBlockchainData() {
+    const web3 = new Web3(Web3.givenProvider || "http://localhost:8545")
+    const accounts = await web3.eth.getAccounts()
+    this.setState({ account: accounts[0] })
+
+    const networkId = await web3.eth.net.getId();
+    const deployedNetwork = StepCoinContract.networks[networkId];
+    const StepCoin = new web3.eth.Contract(
+        StepCoinContract.abi,
         deployedNetwork && deployedNetwork.address,
       );
+    this.setState({ StepCoin});
 
-      // Set web3, accounts, and contract to the state, and then proceed with an
-      // example of interacting with the contract's methods.
-      this.setState({ web3, accounts, contract: instance }, this.runExample);
-    } catch (error) {
-      // Catch any errors for any of the above operations.
-      alert(
-        `Failed to load web3, accounts, or contract. Check console for details.`,
-      );
-      console.error(error);
-    }
-  };
+    // read enroll or not
+    var checkEnrollStatus = await StepCoin.methods.enrolled(accounts[0]).call({from: accounts[0]});
+    console.log(checkEnrollStatus);
+    this.setState({isEnroll: checkEnrollStatus});  
+ 
+    
+    //read balance
+    var currentBalance = await StepCoin.methods.balances(accounts[0]).call({from: accounts[0]});
+    console.log(currentBalance);
+    this.setState({currentBalance});
 
-  runExample = async () => {
-    const { accounts, contract } = this.state;
+    //var doEnroll = await StepCoin.methods.enroll().send({ from: accounts[0]});
+  }
 
-    // Stores a given value, 5 by default.
-    await contract.methods.set(5).send({ from: accounts[0] });
+  async Enroll(){
+    console.log("you did press me");
+    //var ee = await StepCoin.methods.enroll().send({ from: accounts[0] });
+    //console.log(ee);
+  }
 
-    // Get the value from the contract to prove it worked.
-    const response = await contract.methods.get().call();
 
-    // Update state with the result.
-    this.setState({ storageValue: response });
-  };
+
+
 
   render() {
-    if (!this.state.web3) {
-      return <div>Loading Web3, accounts, and contract...</div>;
-    }
     return (
-      <div className="App">
-        <h1>Good to Go!</h1>
-        <p>Your Truffle Box is installed and ready.</p>
-        <h2>Smart Contract Example</h2>
-        <p>
-          If your contracts compiled and migrated successfully, below will show
-          a stored value of 5 (by default).
-        </p>
-        <p>
-          Try changing the value stored on <strong>line 42</strong> of App.js.
-        </p>
-        <div>The stored value is: {this.state.storageValue}</div>
+      <div className="container">
+        <h1>Step Coin dApp (beta)</h1>
+        <p>Your account address: {String(this.state.account).substring(0,6)+ "..."+ String(this.state.account).substring(38)}</p>
+        <p>Your enrollment status: {this.state.isEnroll}</p>
+        <p>Your balance: {this.state.currentBalance}</p>
+          <div id="content">
+          <button onClick={this.doEnroll} >Enroll</button> 
+          <br></br>
+          <p>you click {this.state.count} times</p>
+          <button onClick={() => this.setState({count: this.state.count + 1})}>Count</button> 
+            <form>
+              
+              <input id="claimSteps" type="number" placeholder="put your steps" required />
+                
+              <button id="step-input-button" hidden="" >Submit Your Steps</button>         
+            </form>
+         </div>
       </div>
+      
+      
+
     );
   }
 }
-
 export default App;
